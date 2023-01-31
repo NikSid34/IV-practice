46825ed2dc72cf85a0740b11117500bcb25f690e
Никита <niksid34@mail.ru>
2023-01-31 12:26:31 +0300

    Add 2 controllers to read feedbacks

--- index.php
+++ index.php
@@ -17,4 +17,37 @@ $app->get('/', function (Request $request, Response $response, $args)
 	return $response;
 });
 
+/**
+ * Контроллер, возвращающий отзыв по id
+ */
+$app->get('/api/feedbacks/{id}/', function (Request $request, Response $response, $args)
+{
+	$id = $request->getAttribute('id');
+	$data = (new Feedback())->getFeedback($id);
+	$payload = json_encode($data);
+	$response->getBody()->write($payload);
+	return $response
+		->withHeader('Content-Type', 'application/json');
+});
+
+/**
+ * Контроллер, возвращающий все отзывы с постраничной навигацией
+ */
+$app->get('/api/feedbacks[/{page}]', function (Request $request, Response $response, $args)
+{
+	if($request->getAttribute('page')!=null)
+	{
+		$page = $request->getAttribute('page');
+	}
+	else
+	{
+		$page = 0;
+	}
+	$data = (new Feedback())->getAllFeedbacks($page);
+	$payload = json_encode($data);
+	$response->getBody()->write($payload);
+	return $response
+		->withHeader('Content-Type', 'application/json');
+});
+
 $app->run();
